39423e86ff5d52b5903497ea59131edea69d9fd2
Geotabular <geotabular@gmail.com>
2016-07-27 14:03:22 +1000

    Added filters for advertisements

--- app/js/factories/filterExclusions.js
+++ app/js/factories/filterExclusions.js
@@ -3,10 +3,18 @@ var iflJavaControllers = angular.module('iflJavaControllers');
 
 iflJavaControllers.factory('filterExclusions', ['jsonPath', function(jsonPath){
     return function(rawNewsList, options){
-        return rawNewsList.filter(function(child){               
+        return rawNewsList.filter(function(child){
             return options.exclusions.filter(function(exclusion){
                 var childProp = jsonPath(child, exclusion.path);
-                return childProp !== exclusion.value;
+                if(exclusion.value){
+                  return childProp !== exclusion.value;
+                }
+                if(exclusion.includes){
+                  return exclusion.filter(function(include){
+                    return child.title.includes(include);
+                  }).length != 0;
+                }
+
             }).length > 0;
         });
      };
